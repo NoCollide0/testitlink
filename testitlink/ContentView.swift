c7fb885e53bfc2d89443fd1dc8b91dc460f2e621
2132112313 <2131>
2025-05-05 05:11:16 +0300

    Initial Commit

---
+++ testitlink/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testitlink
+//
+//  Created by Федор Шашков on 05.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testitlink/testitlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  testitlinkApp.swift
+//  testitlink
+//
+//  Created by Федор Шашков on 05.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct testitlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
